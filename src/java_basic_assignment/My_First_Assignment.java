05f661628af204cb059a10fb002dac5380ae3474
jobaid <jobaidazim@gmail.com>
2022-10-08 14:41:14 -0400

    Practice Java

---
+++ src/java_basic_assignment/My_First_Assignment.java
@@ -0,0 +1,34 @@
+package java_basic_assignment;
+
+public class My_First_Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// System.out.println
+		System.out.println("My test 1");
+		System.out.println("My test 2");
+		System.out.println("My test 3");
+		System.out.println("My test 4");
+		System.out.println("My test 5");
+		System.out.println("My test 6");
+		System.out.println("My test 7");
+		System.out.println("My test 8");
+		System.out.println("My test 9");
+		System.out.println("My test 10");
+		
+		//System.out.print
+		System.out.print("My test 1 ");
+		System.out.print("My test 2 ");
+		System.out.print("My test 3 ");
+		System.out.print("My test 4 ");
+		System.out.print("My test 5 ");
+		System.out.print("My test 6 ");
+		System.out.print("My test 7 ");
+		System.out.print("My test 8 ");
+		System.out.print("My test 9 ");
+		System.out.print("My test 10 ");
+		
+
+	}
+
+}
---
+++ src/java_basic_assignment/My_first_Assignment1.java
@@ -0,0 +1,101 @@
+package java_basic_assignment;
+
+public class My_first_Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Int
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int d = 4;
+		int e = 5;
+		int f = 6;
+		int g = 7;
+		int h = 8;
+		int i = 9;
+		int j = 0;
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(i);
+		System.out.println(j);
+		// char
+		char a1 = 'a';
+		char a2 = 'b';
+		char a3 = 'c';
+		char a4 = 'd';
+		char a5 = 'e';
+		char a6 = 'f';
+		char a7 = 'g';
+		char a8 = 'h';
+		char a9 = 'i';
+		char a10 = 'j';
+
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(a4);
+		System.out.println(a5);
+		System.out.println(a6);
+		System.out.println(a7);
+		System.out.println(a8);
+		System.out.println(a9);
+		System.out.println(a10);
+
+		// Double
+		double c1 = 1.4;
+		double c2 = 22.3;
+		double c3 = 1.33;
+		double c4 = 1.5565;
+		double c5 = 1.3434;
+		double c6 = 1.32323;
+		double c7 = 1.43434;
+		double c8 = 1.2334;
+		double c9 = 343.43;
+		double c10 = 1.33569;
+
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		System.out.println(c5);
+		System.out.println(c6);
+		System.out.println(c7);
+		System.out.println(c8);
+		System.out.println(c9);
+		System.out.println(c10);
+
+		// boolean
+		boolean b1 = 4 > 5;
+		boolean b2 = 4 != 5;
+		boolean b3 = 4 == 6;
+		boolean b4 = 4 < 5;
+		boolean b5 = 4 >= 4;
+		boolean b6 = 5 != 3;
+		boolean b7 = 5 == 3;
+		boolean b8 = 4 > 0.5;
+		boolean b9 = 4 < 0.5;
+		boolean b10 = 4 == 0.5;
+
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		System.out.println(b4);
+		System.out.println(b5);
+		System.out.println(b6);
+		System.out.println(b7);
+		System.out.println(b8);
+		System.out.println(b9);
+		System.out.println(b10);
+
+	}
+
+}
